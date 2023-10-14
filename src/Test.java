18e6fbc889c21866fb334d387e898d0acc2d75a6
evgentit <veist91@mail.ru>
2023-10-14 18:12:45 +0300

    test commit Diman

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+    private int i;
+
+    public Test() {
+
+    }
+
+    public Test(int i) {
+        this.i = i;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+}
